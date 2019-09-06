0d8dc88f03188b1e7d5594b5e0d347809f6a42e4
romiinavega <romiivega3005@gmail.com>
2019-09-06 08:45:50 -0700

    Agregar archivos de proyecto.

---
+++ Herencias/Figura.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencias
+{
+    abstract class Figura
+    {
+        public string Color { get; set; }
+        //todas las fiuras deben ser capaces de calcular area
+        public abstract double calcularArea();
+        public string toString()
+        {
+            return "Soy una figura:" + Color;
+        }
+    }
+}
---
+++ Herencias/Triangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencias
+{
+    class Triangulo : Figura
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public override double calcularArea()
+        {
+            return (Base * Altura) / 2.0;
+        }
+        public Triangulo ()
+        {
+            Color = " Amarilla";
+        }
+    }
+}
