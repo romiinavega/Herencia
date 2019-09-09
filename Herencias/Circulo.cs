edf9e769f3572744a56eb493de92f96c1dfaeeca
ROMINAVEGA <romiivega3005@gmail.com>
2019-09-09 12:40:14 -0700

    cambio

---
+++ Herencias/Circulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencias
+{
+    class Circulo : Figura
+    {
+        public double Radio { get; set; }
+        public override double calcularArea()
+        {
+            return Math.PI * (Radio * Radio );
+        }
+        public Circulo(double radio)
+        {
+            Radio = radio;
+            Color = "Sin color";
+        }
+    }
+}
--- Herencias/Program.cs
+++ Herencias/Program.cs
@@ -11,7 +11,28 @@ namespace Herencias
         static void Main(string[] args)
         {
             Triangulo triangulo = new Triangulo();
+            triangulo.Color = "Morado";
+
             Console.WriteLine(triangulo.toString());
+            triangulo.Altura = 2.5;
+            triangulo.Base = 4;
+            double areaTriangulo = triangulo.calcularArea();
+            Console.WriteLine("Area del triangulo: " + areaTriangulo);
+            Circulo circulo = new Circulo(5.4);
+            Console.WriteLine("Area circulo: " + circulo.calcularArea().ToString());
+            Figura triangulo2 = new Triangulo();
+            ((Triangulo)triangulo2).Base = 2;
+            ((Triangulo)triangulo2).Altura = 3;
+            Console.WriteLine("Area del triangulo 2: " + triangulo2.calcularArea().ToString());
+            List<Figura> figuras = new List<Figura>();
+            figuras.Add(triangulo);
+            figuras.Add(circulo);
+            figuras.Add(triangulo2);
+
+            foreach (var figura in figuras)
+            {
+                Console.WriteLine("Area de figura: " + figura.calcularArea().ToString());
+            }
             Console.Read();
         }
     }
